8c3f0802dd259cd39d1960905bbeacc978d8894e
Wonjun Choi <wonjune12@hotmail.com>
2018-12-17 17:19:38 -0500

    randomized button page load, changed orders of image display.

--- main1.js
+++ main1.js
@@ -2,9 +2,11 @@
 
 var clickEvent = document.querySelector("button");
 var showRow = document.querySelector(".row");
+var randomPage = document.getElementById("pokemonPage");
 clickEvent.addEventListener("click",(event)=>{
     localStorage.setItem("first",document.getElementById("trainer-name1").value); 
     localStorage.setItem("second",document.getElementById("trainer-name2").value); 
     showRow.classList.add("slideDown");
+    randomPage.href = "./pokemon"+Math.ceil(Math.random()*6)+".html";
 })
 
